e8564f7404ab4a5477c6c46e34d64a0d696e6f77
Erick Marcos <127636795+Erickz1n@users.noreply.github.com>
2024-09-16 14:04:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+
+setTimeout(function() {
+    window.location.href = "https://api.whatsapp.com/send?phone=5584991147195&text=Figz,%20me%20interessei%20pelo%20seu%20trabalho.%20Podemos%20conversar?"; // Substitua pela URL desejada
+  }, 3000); // 3000 milissegundos = 3 segundos
